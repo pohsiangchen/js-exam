0791ded9a69e9062ffbd61e924b7996e97146bf8
Robert Chen <t7663579@gmail.com>
2019-02-21 13:08:19 +0800

    Updates

---
+++ precache-manifest.e4924df0fe4ebd1287cef3367fdf283c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca96212875be3e7873960295b9b17f0d",
+    "url": "/js-exam/static/media/not-found.ca962128.jpg"
+  },
+  {
+    "revision": "0e05c472e36aea48a322",
+    "url": "/js-exam/static/js/runtime~main.0e05c472.js"
+  },
+  {
+    "revision": "e0d88ff7ff51d564ef8f",
+    "url": "/js-exam/static/js/main.e0d88ff7.chunk.js"
+  },
+  {
+    "revision": "1a1f6041f7c874f2bf4a",
+    "url": "/js-exam/static/js/1.1a1f6041.chunk.js"
+  },
+  {
+    "revision": "e0d88ff7ff51d564ef8f",
+    "url": "/js-exam/static/css/main.4a93e992.chunk.css"
+  },
+  {
+    "revision": "1a1f6041f7c874f2bf4a",
+    "url": "/js-exam/static/css/1.5acc07e5.chunk.css"
+  },
+  {
+    "revision": "8dee41f9a6642e043f5df6251587d840",
+    "url": "/js-exam/index.html"
+  }
+];
